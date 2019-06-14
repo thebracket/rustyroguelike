350b3f6a55920652f30e3beb4f34da5b36154fcb
Herbert Wolverson <herberticus@gmail.com>
2019-06-14 09:19:40 -0500

    Basic FOV calculation.

---
+++ src/game/visibility.rs
@@ -0,0 +1,18 @@
+use crate::rltk;
+use rltk::field_of_view;
+use rltk::Point;
+
+use super::Map;
+use super::Player;
+//use super::Mob;
+
+pub trait Visibility {
+    fn plot_visibility(&mut self, map : &Map);
+}
+
+impl Visibility for Player {
+    fn plot_visibility(&mut self, map: &Map) {
+        let fov_check = |pt : &Point|->bool { map.is_transparent(pt.x, pt.y) };
+        self.visible_tiles = field_of_view(&self.position, 6, &fov_check); 
+    }
+}
---
+++ src/rltk/point.rs
@@ -0,0 +1,10 @@
+pub struct Point {
+    pub x: i32,
+    pub y: i32
+}
+
+impl Point {
+    pub fn new(x:i32, y:i32) -> Point {
+        return Point{x, y};
+    }
+}
