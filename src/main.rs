9a6b04dad9ebb34e82421a56c8cdd3cb146bd5cc
Herbert Wolverson <herberticus@gmail.com>
2019-07-11 15:02:54 -0500

    Fix titlebar.

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ mod game;
 
 fn main() {
     let gs = game::State::new_menu();
-    let mut context = Rltk::init_simple8x8(80, 50, "Hello RLTK World", "resources");
+    let mut context = Rltk::init_simple8x8(80, 50, "Rusty Roguelike", "resources");
     context.with_post_scanlines(true);
     rltk::main_loop(context, Box::new(gs));
 }
