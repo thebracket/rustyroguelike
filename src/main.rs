5fe32c098cdbe277f2dde4a3b87273c5bc7bf4b8
Herbert Wolverson <herberticus@gmail.com>
2019-07-11 15:01:01 -0500

    Update to latest version of RLTK, and enable post-processing FX.

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@ mod game;
 
 fn main() {
     let gs = game::State::new_menu();
-    let context = Rltk::init_simple8x8(80, 50, "Hello RLTK World", "resources");
+    let mut context = Rltk::init_simple8x8(80, 50, "Hello RLTK World", "resources");
+    context.with_post_scanlines(true);
     rltk::main_loop(context, Box::new(gs));
 }
