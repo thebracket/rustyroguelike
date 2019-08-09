a267c52d65b59423a47d5480367be8c77a562c76
Herbert Wolverson <herberticus@gmail.com>
2019-08-09 09:08:25 -0500

    Update to latest RLTK build.

--- src/main.rs
+++ src/main.rs
@@ -6,5 +6,5 @@ fn main() {
     let gs = game::State::new_menu();
     let mut context = Rltk::init_simple8x8(80, 50, "Rusty Roguelike", "resources");
     context.with_post_scanlines(true);
-    rltk::main_loop(context, Box::new(gs));
+    rltk::main_loop(context, gs);
 }
